c74132044ed6c7da9db9b59f6de43ab859bc7eb0
arnaudbey <arnaud.bey@gmail.com>
2013-07-17 16:27:52 +0200

    initial commit

---
+++ InnovaDirectoryBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Innova\DirectoryBundle;
+
+use Claroline\CoreBundle\Library\PluginBundle;
+
+/**
+ * Bundle class.
+ */
+class InnovaDirectoryBundle extends PluginBundle
+{
+}
---
+++ Listener/ToolListener.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Innova\DirectoryBundle\Listener;
+
+use Claroline\CoreBundle\Event\Event\DisplayToolEvent;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class ToolListener extends ContainerAware
+{
+    public function onWorkspaceOpen(DisplayToolEvent $event)
+    {
+        $event->setContent($this->workspace($event->getWorkspace()->getId()));
+    }
+
+    public function onDesktopOpen(DisplayToolEvent $event)
+    {
+        $event->setContent($this->desktop());
+    }
+
+    private function workspace($id)
+    {
+        //if you want to keep the context, you must retrieve the workspace.
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $workspace = $em->getRepository('ClarolineCoreBundle:Workspace\AbstractWorkspace')->find($id);
+
+        return $this->container->get('templating')->render(
+            'InnovaDirectoryBundle::workspaceTool.html.twig', array('workspace' => $workspace)
+        );
+    }
+
+    private function desktop()
+    {
+        return $this->container->get('templating')->render(
+            'InnovaDirectoryBundle::desktopTool.html.twig'
+        );
+    }
+}
